79b3569906e5095cbf91f01a608bf4160706349b
Morfin Overtkill <morfinov@oa-f2.msk.21-school.ru>
2023-10-03 18:43:40 +0300

    mlearnP

--- MLPmnist.cc
+++ MLPmnist.cc
@@ -1,16 +1,19 @@
+#include <chrono>
 #include <cmath>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <random>
+#include <sstream>
 #include <vector>
 
 class NN {
  public:
-  int nnInputSize = 3;
-  int nnHiddenSize_1 = 5;
-  int nnHiddenSize_2 = 5;
-  int nnOutputSize = 2;
-  double learn = 0.25;
+  int nnInputSize = 784;
+  int nnHiddenSize_1 = 1000;
+  int nnHiddenSize_2 = 1000;
+  int nnOutputSize = 26;
+  double learn = 0.025;
 
   std::vector<double> nnInputNeurons;
   std::vector<double> nnHiddenNeurons_1;
@@ -23,7 +26,7 @@ class NN {
 
   std::vector<double> mse;
 
-  NN() { mse.resize(8); }
+  NN() { mse.resize(88800); }
 
   double sigmoid(double x) { return 1 / (1 + exp(-x)); }
   double sigmoidDx(double x) {
@@ -31,7 +34,7 @@ class NN {
     return x * (1.0 - x);
   }
 
-  void feedForward(std::vector<double> input) {
+  void feedForward(std::vector<double> &input) {
     nnInputNeurons = input;
     for (int i = 0; i < nnHiddenSize_1; ++i) {
       double result = 0;
@@ -61,13 +64,13 @@ class NN {
   double genXavier(double x, double y) {
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_real_distribution<double> gen(-(sqrt(6) / sqrt(x + y)),
-                                               (sqrt(6) / sqrt(x + y)));
+    std::uniform_real_distribution<double> gen(
+        -(std::sqrt(6) / std::sqrt(x + y)), (std::sqrt(6) / std::sqrt(x + y)));
     return gen(rng);
   }
 
-  double layerSum(std::vector<double> errors,
-                  std::vector<std::vector<double>> weight, int index) {
+  double layerSum(std::vector<double> &errors,
+                  std::vector<std::vector<double>> &weight, int index) {
     double result = 0;
     for (size_t j = 0; j < weight[index].size(); ++j) {
       result += weight[index][j] * errors[j];
@@ -101,34 +104,30 @@ class NN {
     }
   }
 
-  void train(std::vector<double> answer, std::vector<double> input) {
+  void train(std::vector<double> &answer, std::vector<double> &input) {
     feedForward(input);
-    // std::cout << nnOutputNeurons[0] << std::endl;
 
     std::vector<double> error_1(nnHiddenSize_1);
     std::vector<double> grad_1(nnHiddenSize_1);
     std::vector<double> error_2(nnHiddenSize_2);
     std::vector<double> grad_2(nnHiddenSize_2);
-    // double error_3 = nnOutputNeurons[nIndex] - answer;
-    // double grad_3 = sigmoidDx(nnOutputNeurons[nIndex]);
-    // double weights_delta_3 = error_3 * grad_3;
+
     std::vector<double> error_3(nnOutputSize);
     std::vector<double> grad_3(nnOutputSize);
 
-    for (size_t i = 0; i < grad_3.size(); ++i) {
+    for (int i = 0; i < nnOutputSize; ++i) {
       grad_3[i] = sigmoidDx(nnOutputNeurons[i]);
     }
 
-    for (size_t i = 0; i < grad_2.size(); ++i) {
+    for (int i = 0; i < nnHiddenSize_2; ++i) {
       grad_2[i] = sigmoidDx(nnHiddenNeurons_2[i]);
     }
 
-    for (size_t i = 0; i < grad_1.size(); ++i) {
+    for (int i = 0; i < nnHiddenSize_1; ++i) {
       grad_1[i] = sigmoidDx(nnHiddenNeurons_1[i]);
     }
 
     for (size_t i = 0; i < error_3.size(); ++i) {
-      // error_3[i] = pow(answer[i] - nnOutputNeurons[i], 2) / 2 * grad_3[i];
       error_3[i] = nnOutputNeurons[i] - answer[i];
     }
 
@@ -141,56 +140,128 @@ class NN {
     }
 
     for (int i = 0; i < nnHiddenSize_2; ++i) {
+      double tmp = nnHiddenNeurons_2[i] * learn;
       for (int j = 0; j < nnOutputSize; ++j) {
-        nnHiddenOutputWeight[i][j] -=
-            nnHiddenNeurons_2[i] * error_3[j] * grad_3[j] * learn;
+        nnHiddenOutputWeight[i][j] -= error_3[j] * grad_3[j] * tmp;
       }
     }
 
     for (int i = 0; i < nnHiddenSize_1; ++i) {
+      double tmp = nnHiddenNeurons_1[i] * learn;
       for (int j = 0; j < nnHiddenSize_2; ++j) {
-        nnHiddenHiddenWeight[i][j] -=
-            nnHiddenNeurons_1[i] * error_2[j] * grad_2[j] * learn;
+        nnHiddenHiddenWeight[i][j] -= error_2[j] * grad_2[j] * tmp;
       }
     }
 
     for (int i = 0; i < nnInputSize; ++i) {
+      double tmp = nnInputNeurons[i] * learn;
       for (int j = 0; j < nnHiddenSize_1; ++j) {
-        nnInputHiddenWeight[i][j] -=
-            nnInputNeurons[i] * error_1[j] * grad_1[j] * learn;
+        nnInputHiddenWeight[i][j] -= error_1[j] * grad_1[j] * tmp;
       }
     }
   }
 };
 
-void print(std::vector<double> x) {
+void print(std::vector<double> &x) {
   for (size_t i = 0; i < x.size(); ++i) {
     std::cout << x[i] << " ";
   }
 }
 
-bool results(std::vector<double> ans, std::vector<double> out) {
-  int ansIndex;
-  bool res = true;
+bool results(std::vector<double> &ans, std::vector<double> &out) {
+  int ansIndex = 0;
   for (size_t i = 0; i < ans.size(); ++i) {
     if (ans[i] == 1.0) {
       ansIndex = i;
       break;
     }
   }
-  if (ans[ansIndex] - out[ansIndex] > 0.2) res = false;
-  return res;
+  double max = 0;
+  int outIndex = 0;
+  for (size_t i = 0; i < out.size(); ++i) {
+    if (out[i] > max) {
+      max = out[i];
+      outIndex = i;
+    }
+  }
+  return (ansIndex == outIndex) ? true : false;
+}
+
+void parseData(const std::string &file, std::vector<std::vector<double>> &vect,
+               std::vector<std::vector<double>> &answers) {
+  std::ifstream infile(file);
+  std::string line;
+  std::string temp;
+  int t = 0;
+
+  while (std::getline(infile, line)) {
+    bool ans = true;
+    std::istringstream ss(line);
+    while (std::getline(ss, temp, ',')) {
+      if (ans) {
+        answers[t][std::stod(temp) - 1] = 1;
+        ans = false;
+      } else {
+        vect[t].push_back(std::stod(temp) / 255);
+      }
+    }
+    ++t;
+  }
+  infile.close();
+}
+
+double mean(std::vector<double> &out, std::vector<double> &ans) {
+  double result = 0;
+  for (size_t i = 0; i < out.size(); ++i) {
+    result += std::pow(out[i] - ans[i], 2);
+  }
+  return result;
+}
+
+void accur(std::vector<std::vector<double>> &data,
+           std::vector<std::vector<double>> &ans, NN one) {
+  int acc = 0;
+  for (size_t i = 0; i < ans.size(); ++i) {
+    one.feedForward(data[i]);
+    // std::cout << "Input - ";
+    // print(trainset[i]);
+    // std::cout << "expected - ";
+    // print(answerset[i]);
+    // std::cout << "predict - ";
+    // print(one.nnOutputNeurons);
+    if (results(ans[i], one.nnOutputNeurons)) {
+      ++acc;
+      // std::cout << "SUCCESS";
+    } else {
+      // std::cout << "FAILED";
+    }
+    // std::cout << std::endl;
+  }
+  std::cout << std::setprecision(1) << acc / (double)ans.size() * 100 << "%"
+            << std::endl;
 }
 
 int main() {
   NN one;
   int epoch = 0;
-  std::vector<std::vector<double>> trainset = {
-      {0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1},
-      {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1},
-  };
-  std::vector<std::vector<double>> answerset = {{1, 0}, {0, 1}, {1, 0}, {1, 0},
-                                                {0, 1}, {0, 1}, {1, 0}, {0, 1}};
+  // std::vector<std::vector<double>> trainset = {
+  //     {0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1},
+  //     {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1},
+  // };
+  std::vector<std::vector<double>> trainset(88800);
+  std::vector<std::vector<double>> answerset(88800, std::vector<double>(26));
+  parseData("/Users/morfinov/Downloads/emnist-letters/emnist-letters-train.csv",
+            trainset, answerset);
+  std::vector<std::vector<double>> testset(14800);
+  std::vector<std::vector<double>> answersetTest(14800,
+                                                 std::vector<double>(26));
+  parseData("/Users/morfinov/Downloads/emnist-letters/emnist-letters-test.csv",
+            testset, answersetTest);
+
+  // std::vector<std::vector<double>> answerset = {{1, 0}, {0, 1}, {1, 0},
+  // {1, 0},
+  //                                               {0, 1}, {0, 1}, {1, 0},
+  //                                               {0, 1}};
   // std::vector<std::vector<double>> answerset = {{0}, {1}, {0}, {0},
   //                                               {1}, {1}, {0}, {1}};
   one.nnInputNeurons.resize(one.nnInputSize);
@@ -219,14 +290,15 @@ int main() {
   // one.nnHiddenOutputWeight[0][0] = 0.5;
   // one.nnHiddenOutputWeight[1][0] = 0.52;
 
-  while (epoch < 70) {
+  while (epoch < 20) {
+    auto t1 = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < answerset.size(); ++i) {
       one.train(answerset[i], trainset[i]);
-      one.mse[i] = ((pow(one.nnOutputNeurons[0] - answerset[i][0], 2)) +
-                    pow(one.nnOutputNeurons[1] - answerset[i][1], 2)) /
-                   one.nnOutputSize;
-      // std::cout << one.nnOutputNeurons[0] << std::endl;
+      one.mse[i] = mean(one.nnOutputNeurons, answerset[i]) / one.nnOutputSize;
     }
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count();
     // one.feedForward({0, 1, 1});
     // std::cout << one.nnOutputNeurons[0] << std::endl;
     epoch++;
@@ -236,24 +308,11 @@ int main() {
       std::cout << epoch << std::fixed << std::setprecision(4)
                 << " epoch has ended " << std::endl;
       std::cout << std::fixed << std::setprecision(4) << "error - "
-                << std::reduce(one.mse.begin(), one.mse.end()) / 8 << std::endl;
+                << std::reduce(one.mse.begin(), one.mse.end()) / 88800
+                << std::endl;
+      std::cout << "time - " << duration << std::endl;
+      accur(testset, answersetTest, one);
     }
-  }
-
-  for (size_t i = 0; i < answerset.size(); ++i) {
-    one.feedForward(trainset[i]);
-    // std::cout << "Input - ";
-    // print(trainset[i]);
-    // std::cout << "expected - ";
-    // print(answerset[i]);
-    // std::cout << "predict - ";
-    // print(one.nnOutputNeurons);
-    if (results(answerset[i], one.nnOutputNeurons)) {
-      std::cout << "SUCCESS";
-    } else {
-      std::cout << "FAILED";
-    }
-    std::cout << std::endl;
   }
 
   // std::cout << one.nnOutputNeurons[0] << std::endl;
