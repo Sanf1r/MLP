db3c02bee710276faf2bb9a68ed2b5d2a2aad968
Morfin Overtkill <sanfir@yandex.ru>
2023-11-09 12:21:50 +0300

    looking good

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MLP
 Implementation of a basic multilayer perceptron.
+
+Tested on MacOS Big Sur 11.6.6.
+
+Made on October 25 2023 as part of my education in School 21 :)
--- src/main.cc
+++ src/main.cc
@@ -9,10 +9,8 @@ int main(int argc, char *argv[]) {
   std::locale::global(std::locale("C"));
   QLocale curLocale(QLocale("C"));
   QLocale::setDefault(curLocale);
-
   s21::Model model;
   s21::Controller controller(&model);
-
   s21::View view(&controller);
   view.show();
   return a.exec();
